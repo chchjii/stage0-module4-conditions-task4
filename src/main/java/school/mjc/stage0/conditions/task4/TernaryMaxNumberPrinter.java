e7eba2b1fe7cad0a7d171d42960c07d5de9d9fd9
Kirill <cryt.mem@gmail.com>
2025-08-15 10:33:22 +0300

    sol

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        System.out.println(first > second && first > third ? first : second > first && second > third ? second : third > first && third > second ? third : first);
+
+
+
+        /*if (first > second && first > third){
+            System.out.println(first);
+        } else if (second > first && second > third) {
+            System.out.println(second);
+        } else if (third > first && third > second) {
+            System.out.println(third);
+        }*/
     }
 }
